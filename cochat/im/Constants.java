dd9a46a515c0330f529af8e53c77a01506e2c2d7
su <35282822+sushome@users.noreply.github.com>
2018-05-11 17:07:00 +0800

    Update Constants.java

--- cochat/im/Constants.java
+++ cochat/im/Constants.java
@@ -5,8 +5,8 @@ public class Constants {
 	public final String START_PAGE = COMMON_PART + "fragment_img";
 	public final String USERNAME_FIELD = COMMON_PART + "usernameEditText";
 	public final String PASSWORD_FIELD = COMMON_PART + "pwdEditText";
-	public final String USERNAME = "susm2";
-	public final String PASSWORD = "uEoY-2384";
+	public final String USERNAME = "username";
+	public final String PASSWORD = "password";
 	public final String LOGIN_BUTTON = COMMON_PART + "loginButton";
 	
 	public final String ADD_BUTTON = COMMON_PART + "iv_main_right";
